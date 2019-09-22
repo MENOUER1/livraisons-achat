3b15d56465fd3f575726264689f8804eb363107b
MENOUER1 <menouerr2@gmail.com>
2019-09-22 22:00:11 +0200

    changed.files

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const button = document.querySelector("button");
+
+const imput  = document.querySelector("imput")
+
+const ul = document.querySelector("ul");
+
+let li = document.getElementByNameli("li")
+
+
+function createElementLi() {
+    li = document.createElement('li');
+}
+
+
+function insertLi() {
+    ul.insertAdjacentElement("beforeend", li);
+}
+
+
+
+
+function resetinput() { input => 0
+    
+}
+    
+
+
+
+    function addImputElement
+
+    li.append(document.createTextNode(imput.value));
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
